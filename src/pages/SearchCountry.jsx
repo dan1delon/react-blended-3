fc81d4bb743603509b4c52d30c9bd386541bf002
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-04-06 16:00:39 +0300

    update SearchCountry

--- src/pages/SearchCountry.jsx
+++ src/pages/SearchCountry.jsx
@@ -1,10 +1,43 @@
-import { Container, Heading, Section } from 'components';
+import {
+  Container,
+  CountryList,
+  Heading,
+  Loader,
+  SearchForm,
+  Section,
+} from 'components';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByRegion } from 'service/countryApi';
 
 export const SearchCountry = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const region = searchParams.get('region');
+    if (!region) return;
+
+    setIsLoading(true);
+    fetchByRegion(region)
+      .then(countries => setCountries(countries))
+      .catch(error => setError(error.message))
+      .finally(() => setIsLoading(false));
+  }, [searchParams]);
+
+  const searchCountry = region => {
+    setSearchParams({ region });
+  };
+
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        <SearchForm searchCountry={searchCountry} />
+        <CountryList countries={countries} />
+        {isLoading && <Loader />}
+        {error && <Heading title={error} />}
       </Container>
     </Section>
   );
